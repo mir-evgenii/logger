a347214cd7bf8156e266669dec3a56d6c4723f91
Eugene Mironov <mir-evgenii@mail.ru>
2018-12-20 17:58:33 +0300

    GUI main window it's ok

--- GUI.py
+++ GUI.py
@@ -9,10 +9,15 @@ class GUI:
         self.login = None
         self.password = None
         self.host = None
-        self.find_text = None
+        self.find_number = [None, ]
+        self.find_date = None
+        self.find_text_add = []
         self.ssh = None
         self.result = None
         self.win = None
+        self.btn_del_ent = None
+        self.btn_frame = None
+        self.ent_frame = None
 
         self.root = tkinter.Tk()
         self.root.title("Logger")
@@ -36,6 +41,7 @@ class GUI:
         self.login.insert(0, 'oper')  # значение поля по умолчанию
         password_text = tkinter.Label(self.root, text="Введите пароль", font="Arial 11")  # строка текста
         self.password = tkinter.Entry(self.root, width=20, bd=3, show='*')
+        self.password.focus_set()
 
         btn = tkinter.Button(self.root, text="Войти")
         btn.bind("<Button-1>", self.connect)  # связь кнопки и функции
@@ -55,23 +61,56 @@ class GUI:
 
     def main_window(self, title):
         self.win = tkinter.Toplevel(self.root)
+        self.ent_frame = tkinter.Frame(self.win)
+        self.btn_frame = tkinter.Frame(self.win)
         self.win.title(title)
         self.win.minsize(width=400, height=200)
         text = tkinter.Label(self.win, text="Искать", font="Arial 9")
-        self.find_text = tkinter.Entry(self.win, width=20, bd=3)
-        btn = tkinter.Button(self.win, text="Найти")
+        self.find_number[0] = tkinter.Entry(self.ent_frame, width=20, bd=3)
+        self.find_number[0].focus_set()
+        self.find_date = tkinter.Entry(self.ent_frame, width=20, bd=3)
+        btn = tkinter.Button(self.btn_frame, text="Найти")
         btn.bind("<Button-1>", self.grep)
         self.win.bind("<Return>", self.grep)
         self.result = tkinter.Label(self.win, text="Результат поиска", font="Arial 9")
+        btn_add_ent = tkinter.Button(self.btn_frame, text="Добавить критерий поиска")
+        btn_add_ent.bind("<Button-1>", self.add_ent)
 
         text.pack(padx=5, pady=5)
-        self.find_text.pack(padx=5, pady=5)
-        btn.pack(padx=5, pady=5)
+        self.find_date.pack(side=tkinter.LEFT, padx=5, pady=5)
+        self.find_number[0].pack(side=tkinter.LEFT, padx=5, pady=5)
+        self.ent_frame.pack()
+        self.btn_frame.pack()
+        btn.pack(side=tkinter.LEFT, padx=5, pady=5)
+        btn_add_ent.pack(side=tkinter.LEFT, padx=5, pady=5)
         self.result.pack(padx=5, pady=5)
 
     def grep(self, event):
-        number = self.find_text.get()
-        result = self.ssh.grep(number, 'log', '20181217')
+        date = self.find_date.get()
+
+        for i in date:
+            if i in "1234567890":
+                pass
+            else:
+                tkinter.messagebox.showerror("Ошибка!", "В первое поле нужно ввести дату в формате: ГГГГММДД")
+
+        number = self.find_number[0].get()
+        result = self.ssh.grep(number, 'log', date)
         self.result.pack_forget()
         self.result = tkinter.Label(self.win, text=result, font="Arial 9")
         self.result.pack(padx=5, pady=5)
+
+    def add_ent(self, event):
+        self.find_text_add.append(tkinter.Entry(self.ent_frame, width=20, bd=3))
+        for i in self.find_text_add:
+            i.pack(side=tkinter.LEFT, padx=5, pady=5)
+        if len(self.find_text_add) == 1:
+            self.btn_del_ent = tkinter.Button(self.btn_frame, text="Удалить критерий поиска")
+            self.btn_del_ent.bind("<Button-1>", self.del_ent)
+            self.btn_del_ent.pack(side=tkinter.LEFT, padx=5, pady=5)
+
+    def del_ent(self, event):
+        self.find_text_add[-1].pack_forget()
+        del(self.find_text_add[-1])
+        if len(self.find_text_add) < 1:
+            self.btn_del_ent.pack_forget()
